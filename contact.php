57ce5b0b41a8214ace6996bc9b48a8b8cda3ae8c
Martin92571 <zegrizo@gmail.com>
2018-08-07 12:15:27 -0700

    changed file structure so to be only one template

--- contact.php
+++ contact.php
@@ -1,62 +1,48 @@
-<?php 
-$emailTo = 'yourmail@example.com';
-$siteTitle = 'YourSiteTitle';
+<?php
 
-error_reporting(E_ALL ^ E_NOTICE); // hide all basic notices from PHP
+// configure
+$from = 'Demo contact form <demo@domain.com>';
+$sendTo = 'Demo contact form <demo@domain.com>'; // Add Your Email
+$subject = 'New message from contact form';
+$fields = array('name' => 'Name', 'subject' => 'Subject', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in the email
+$okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
+$errorMessage = 'There was an error while submitting the form. Please try again later';
 
-//If the form is submitted
-if(isset($_POST['submitted'])) {
-	
-	// require a name from user
-	if(trim($_POST['contactName']) === '') {
-		$nameError =  'Forgot your name!'; 
-		$hasError = true;
-	} else {
-		$name = trim($_POST['contactName']);
-	}
-	
-	// need valid email
-	if(trim($_POST['email']) === '')  {
-		$emailError = 'Forgot to enter in your e-mail address.';
-		$hasError = true;
-	} else if (!preg_match("/^[[:alnum:]][a-z0-9_.-]*@[a-z0-9.-]+\.[a-z]{2,4}$/i", trim($_POST['email']))) {
-		$emailError = 'You entered an invalid email address.';
-		$hasError = true;
-	} else {
-		$email = trim($_POST['email']);
-	}
-		
-	// we need at least some content
-	if(trim($_POST['comments']) === '') {
-		$commentError = 'You forgot to enter a message!';
-		$hasError = true;
-	} else {
-		if(function_exists('stripslashes')) {
-			$comments = stripslashes(trim($_POST['comments']));
-		} else {
-			$comments = trim($_POST['comments']);
-		}
-	}
-		
-	// upon no failure errors let's email now!
-	if(!isset($hasError)) {
-		
-		$subject = 'New message to '.$siteTitle.' from '.$name;
-		$sendCopy = trim($_POST['sendCopy']);
-		$body = "Name: $name \n\nEmail: $email \n\nMessage: $comments";
-		$headers = 'From: ' .' <'.$email.'>' . "\r\n" . 'Reply-To: ' . $email;
+// let's do the sending
 
-		mail($emailTo, $subject, $body, $headers);
-		
-        //Autorespond
-		$respondSubject = 'Thank you for contacting '.$siteTitle;
-		$respondBody = "Your message to $siteTitle has been delivered! \n\nWe will answer back as soon as possible.";
-		$respondHeaders = 'From: ' .' <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $emailTo;
-		
-		mail($email, $respondSubject, $respondBody, $respondHeaders);
-		
-        // set our boolean completion value to TRUE
-		$emailSent = true;
-	}
+try
+{
+    $emailText = "You have new message from contact form\n=============================\n";
+
+    foreach ($_POST as $key => $value) {
+
+        if (isset($fields[$key])) {
+            $emailText .= "$fields[$key]: $value\n";
+        }
+    }
+
+    $headers = array('Content-Type: text/plain; charset="UTF-8";',
+        'From: ' . $from,
+        'Reply-To: ' . $from,
+        'Return-Path: ' . $from,
+    );
+    
+    mail($sendTo, $subject, $emailText, implode("\n", $headers));
+
+    $responseArray = array('type' => 'success', 'message' => $okMessage);
 }
-?>
+catch (\Exception $e)
+{
+    $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+}
+
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    $encoded = json_encode($responseArray);
+
+    header('Content-Type: application/json');
+
+    echo $encoded;
+}
+else {
+    echo $responseArray['message'];
+}
