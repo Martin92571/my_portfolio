6cf5c7b58fb85039982c0506a8f92b895764d5ae
Martin92571 <zegrizo@gmail.com>
2018-08-07 00:34:54 -0700

    added template

---
+++ php_mailer/email_config.php
@@ -0,0 +1,6 @@
+<?php
+define('EMAIL_USER','martinphpserver@gmail.com');
+define('EMAIL_PASS','Caca92571');
+define('EMAIL_USERNAME','Martin Portfolio');
+define('EMAIL_TO_ADDRESS','zegrizo@gmail.com');
+?>
