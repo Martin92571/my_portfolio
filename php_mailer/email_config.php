b73328ca559caf779ec6bfa961a4649fa9fb8c84
Martin Rizo <35642313+Martin92571@users.noreply.github.com>
2018-08-31 22:13:34 -0700

    email template

--- php_mailer/email_config.php
+++ php_mailer/email_config.php
@@ -1,6 +1,6 @@
 <?php
-define('EMAIL_USER','martinphpserver@gmail.com');
-define('EMAIL_PASS','Caca92571');
-define('EMAIL_USERNAME','Martin Portfolio');
-define('EMAIL_TO_ADDRESS','zegrizo@gmail.com');
+define('EMAIL_USER','Your Email');
+define('EMAIL_PASS','Your password');
+define('EMAIL_USERNAME','your Portfolio');
+define('EMAIL_TO_ADDRESS','your mail forwords');
 ?>
