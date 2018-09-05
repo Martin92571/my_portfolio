24f576285f2fbd710ede15801b0750b75f6dee2c
Martin92571 <zegrizo@gmail.com>
2018-09-04 17:32:08 -0700

    fixed overflow issue

--- js/contact.js
+++ js/contact.js
@@ -97,8 +97,7 @@ $(document).ready(function() {
 					$('#contact-form .contact-form-message').remove();
 				$('#contact-form').append('<p style="color:rgb(170, 8, 8);font-weight:700;text-shadow: 0px 0px 5px black;" class="contact-form-message">'+'Failed To Send!'+'</p>');
 				$('#contact-form').removeClass('clicked');
-				$('#contact-form')[0].reset();
-				$('#contact-form .form-control').removeClass('input-filled');
+				
 				},2100);
 				
 			});
